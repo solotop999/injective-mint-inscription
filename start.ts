05761e1aa8a9f687f73d2449d4836f4463f02b7f
solotop999 <nhd096@gmail.com>
2023-12-21 17:19:00 +0700

    Update start.ts

--- start.ts
+++ start.ts
@@ -4,40 +4,35 @@ import { BigNumberInBase} from '@injectivelabs/utils'
 import { ChainId } from '@injectivelabs/ts-types'
 
 //////// SETTING HERE ////////////////
-
 const MY_KEYS = 'nasty jelly during tent mixture usual winner grace almost talent glow educate'
 const TOTAL_TX = 10000; // change to the number of transactions you want
+
 const FEE = 0.000111 // $inj
 const GAS = '400000'
 
-const GRPC = "https://grpc.injective.network"
-
+const GRPC = "https://sentry.chain.grpc-web.injective.network:443"
 const restEndpoint ='https://lcd.injective.network'
-// injective-grpc.publicnode.com:443
 ////////////////////////////////////////////////////////
-
-
 
 const MEMO = "ZGF0YToseyJwIjoiaW5qcmMtMjAiLCJvcCI6Im1pbnQiLCJ0aWNrIjoiSU5KUyIsImFtdCI6IjEwMDAifQ=="
 
-// const RPC = 'https://injective-rpc.w3coins.io'
-
-//gRPC: 9091
-//RPC: 26657
-//lcd: 10337
-
 const ChainID = ChainId.Mainnet
 
-const prepareAccount = async (mnemonic: string) => {
+const prepareAccount =  (mnemonic: string) => {
     return PrivateKey.fromMnemonic(mnemonic)
   }
+
+const wallet = prepareAccount(MY_KEYS)
+const myAddress = wallet.toAddress().toBech32();
+console.log(`Address:${myAddress}\n `)
+
+
+
 
   async function start() {
     for(let count=0; count <= TOTAL_TX; count++){
         try {
-            const wallet = await prepareAccount(MY_KEYS)
-            const myAddress = wallet.toAddress().toBech32();
-    
+
             const chainRestAuthApi = new ChainRestAuthApi(restEndpoint);
             const accountDetailsResponse = await chainRestAuthApi.fetchAccount(myAddress);
             const baseAccount = BaseAccount.fromRestApi(accountDetailsResponse);
@@ -73,16 +68,15 @@ const prepareAccount = async (mnemonic: string) => {
             if (txResponse.code !== 0) {
                 console.log(`Transaction failed: ${txResponse.rawLog}`);
             } else {
-                console.log(` - ${count}. Address:${myAddress}\n - Tx hash: https://explorer.injective.network/transaction/${txResponse.txHash}`);
+                console.log(` - ${count}. Tx hash: https://explorer.injective.network/transaction/${txResponse.txHash}`);
             }
         } catch (error) {
-            console.log(`Maybe gRPC Broked: ${GRPC}`)
-            console.log(`     ${error}`)
+            console.log(`Maybe gRPC Broked: ${GRPC}\n${error}`)
         }
     }
 
 }
-  
+
+
 
 start();
-
